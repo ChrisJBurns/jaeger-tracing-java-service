25abff447890ba8f13192af02cb362e6189f4416
ChrisJBurns <29541485+ChrisJBurns@users.noreply.github.com>
2020-06-15 21:28:31 +0100

    Adds basic skeleton for Java SpringBoot app

---
+++ src/main/java/com/jaegartracingjavaservice/JaegerTracingJavaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jaegartracingjavaservice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaegerTracingJavaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaegerTracingJavaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaegartracingjavaservice/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaegartracingjavaservice.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaegerTracingJavaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
